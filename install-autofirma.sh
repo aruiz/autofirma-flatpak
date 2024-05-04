f41c08c57d42af9bc00078869302f6bc155c4bb3
Philippe Normand <philn@igalia.com>
2024-05-04 12:09:58 +0100

    Add desktop file
    Also make the icon square.

--- install-autofirma.sh
+++ install-autofirma.sh
@@ -10,4 +10,9 @@ install -Dm755 deb-package/usr/bin/autofirma                                /app
 install -Dm644 deb-package/usr/lib/AutoFirma/AutoFirma.jar                  /app/usr/lib/AutoFirma/AutoFirma.jar
 install -Dm644 deb-package/usr/lib/AutoFirma/AutoFirma.png                  /app/usr/lib/AutoFirma/AutoFirma.png
 install -Dm644 deb-package/usr/lib/AutoFirma/AutoFirmaConfigurador.jar      /app/usr/lib/AutoFirma/AutoFirmaConfigurador.jar
-install -Dm644 deb-package/usr/share/AutoFirma/AutoFirma.svg                /app/usr/share/AutoFirma/AutoFirma.svg
+
+mkdir -p /app/share/icons/hicolor/scalable/
+install -Dm644 AutoFirma.svg                                                /app/share/icons/hicolor/scalable/com.github.aruiz.Autofirma.svg
+
+mkdir -p /app/share/applications/
+install -Dm644 com.github.aruiz.Autofirma.desktop                           /app/share/applications/com.github.aruiz.Autofirma.desktop
